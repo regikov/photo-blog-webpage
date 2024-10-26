482ff8c61f5fd13ebba70b990101bbec44598598
Your Name <yourname@example.com>
2024-10-26 09:49:13 +0200

    FIXES: photos, photogrid, html

--- portfolio.js
+++ portfolio.js
@@ -8,18 +8,15 @@ function validateForm() {
 
     let isValid = true;
 
-    // Reset error messagesl it is important ex in case user put in wront data, it cleares it up
     nameError.innerHTML = "";
     emailError.innerHTML = "";
     messageError.innerHTML = "";
 
-    // Validate name
     if (name === "") {
         nameError.innerHTML = "Name is required";
         isValid = false;
     }
 
-    // Validate email
     let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailPattern.test(email)) {
         emailError.innerHTML = "Invalid email address";
@@ -30,9 +27,8 @@ function validateForm() {
         messageError.innerHTML="Please type in a message"
         isValid=false
     }
-    // If form is valid, display success message
+
     if (isValid) {
         alert("Form submitted successfully!");
-        // You can also submit the form to a server here using AJAX or by setting the form's action attribute
     }
 }
